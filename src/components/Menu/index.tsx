ee4e3b74b3521519e6a64b96ca9ee27b86a773b5
Jose Manuel Rosa Moncayo <josemanuel.rosamoncayo+github@gmail.com>
2024-09-26 18:05:48 +0200

    feature: add menu component with items

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,43 @@
+import type { PropsWithChildren } from 'react'
+import {
+  Card,
+  List,
+  ListItem,
+  Typography,
+} from '@material-tailwind/react'
+import { Link } from '../Link'
+
+export function Menu({ children }: PropsWithChildren) {
+  return (
+    <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5 rounded-none bg-gray-100">
+      <div className="mb-2 p-4">
+        <Typography variant="h5" color="blue-gray">
+          Menu
+        </Typography>
+      </div>
+
+      <List>
+        {children}
+      </List>
+    </Card>
+  )
+}
+
+interface MenuItemProps {
+  to: string
+  text: string
+}
+
+export function MenuItem({ to, text }: MenuItemProps) {
+  return (
+    <Link to={to}>
+      {({ isActive }) => (
+        <ListItem selected={isActive} className="py-8">
+          {text}
+        </ListItem>
+      )}
+    </Link>
+  )
+}
+
+Menu.item = MenuItem
