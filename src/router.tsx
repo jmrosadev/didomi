8b7e60a8d50f64f7077e577ef09809eb69809eda
Jose Manuel Rosa Moncayo <josemanuel.rosamoncayo+github@gmail.com>
2024-09-26 19:05:23 +0200

    feat: connect components with routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,18 @@
+import { HomeLayout } from '@/layouts/Home'
+import { Consents } from '@/pages/Consents'
+
+import { GiveConsent } from '@/pages/GiveConsent'
+import { NotFound } from '@/pages/NotFound'
+
 import { createElement } from 'react'
-import { createBrowserRouter, Navigate, Outlet, type RouteObject, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Navigate, type RouteObject, RouterProvider } from 'react-router-dom'
 
 export const routes: RouteObject[] = [
   {
     id: 'root',
     path: '/',
-    Component: () => <Outlet />,
-    errorElement: <p>Error</p>,
+    Component: HomeLayout,
+    errorElement: <NotFound />,
     children: [
       {
         index: true,
@@ -14,11 +20,11 @@ export const routes: RouteObject[] = [
       },
       {
         path: '/give-consent',
-        Component: () => <p>Give Consent</p>,
+        Component: GiveConsent,
       },
       {
         path: '/consents',
-        Component: () => <p>Consents</p>,
+        Component: Consents,
       },
     ],
   },
